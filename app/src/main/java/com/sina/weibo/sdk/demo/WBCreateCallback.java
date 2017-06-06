c15befd80c583c0509ea168ad0470a3273ae0436
chengzhong <chengzhong@staff.weibo.com>
2017-06-06 11:48:59 +0800

    微博直播sdk demo第一次提交，用于第三方app参照使用

---
+++ app/src/main/java/com/sina/weibo/sdk/demo/WBCreateCallback.java
@@ -0,0 +1,11 @@
+package com.sina.weibo.sdk.demo;
+
+/**
+ * Created by jinya3 on 2017/5/25.
+ */
+
+public class WBCreateCallback {
+    public static String LIVEID = "";
+    public static String URL="";
+    public static String ROOMID="";
+}
